c6339f890326a4ec3ff75f9276340af241c47e41
michael <michaelhuggins@protonmail.com>
2021-10-27 09:03:05 -0700

    added docker scratch. it is for running localy as developlment env. It is ommited from the build yaml on purpose

--- app_ept.py
+++ app_ept.py
@@ -1,41 +1,57 @@
 import argparse
 import os
-#import pdal
+
+# import pdal
 from string import Template
 import json
 import subprocess
 from pprint import pprint
+import geopandas as gpd
+from shapely.geometry import Polygon
 
-#TODO un-hardcode threads in make_pipe
+# TODO:
+#     Allow for passage of shp, gpgk, geojson, or tiff instead of bbox
+#     find epsg from ept so it needn't be passed
+#     Allow for reprojection
+#     un-hardcode threads in make_pipe
 #     un-hardcode resolution in make_pipe
 #     make docstrings
 
+
+def bbox_from_vector(f, layer=None):
+    """Returns minx, maxx, miny, maxy of vector file or layer"""
+    v = gpd.read_file(f, layer=layer)
+    x, y = v.geometry.envelope.exterior.values[0].coords.xy
+    return (min(x), max(x), min(y), max(y))
+
+
 def ept_window_query(xmin, xmax, ymin, ymax, ept, srs, outpath, tag=None):
-    ''' '''
+    """ """
 
     # make a tag for the output file
-    loc = f'{int(xmin)}_{int(xmax)}_{int(ymin)}_{int(ymax)}'
+    loc = f"{int(xmin)}_{int(xmax)}_{int(ymin)}_{int(ymax)}"
     if tag:
-        f = f'{loc}_{tag}'
+        f = f"{loc}_{tag}"
     else:
-        f = f'{loc}'
-    of = os.path.join(outpath, f + '.las')
+        f = f"{loc}"
+    of = os.path.join(outpath, f + ".las")
 
     # make pipeline
     bbox = ([xmin, xmax], [ymin, ymax])
     pipeline = make_pipe(ept, bbox, of, srs)
-    json_file = os.path.join(outpath, f'{f}.json')
-    with open(json_file, 'w') as j:
+    json_file = os.path.join(outpath, f"{f}.json")
+    with open(json_file, "w") as j:
         json.dump(pipeline, j)
-    
-    #make pdal comand
-    cmd = f'pdal pipeline -i {json_file} --developer-debug'
+
+    # make pdal comand
+    cmd = f"pdal pipeline -i {json_file} --developer-debug"
     _ = subprocess.run(cmd, shell=True, capture_output=True)
-    print(f'stdout:\n{_.stdout}\n\n\nerrors:\n{_.stderr}') 
+    print(f"stdout:\n{_.stdout}\n\n\nerrors:\n{_.stderr}")
+
 
 def make_pipe(ept, bbox, out_path, srs, threads=4, resolution=1):
-    '''Creates, validates and then returns the pdal pipeline
-    
+    """Creates, validates and then returns the pdal pipeline
+
     Arguments:
     ept        -- String -Path to ept file.
     bbox       -- Tuple  - Bounding box in srs coordintes,
@@ -45,79 +61,83 @@ def make_pipe(ept, bbox, out_path, srs, threads=4, resolution=1):
                   because that is what ept files tend to use.
     threads    -- Int    - Number os threads to be used by the reader.ept. Defaults to 4.
     resolution -- Int or Float - resolution (srs units) used by writers.gdal
-    '''
+    """
 
     pipe = {
         "pipeline": [
             {
-            "bounds": f'{bbox}',
-            "filename": ept,
-            "type": "readers.ept",
-            "tag": "readdata",
-            "spatialreference": srs,
-            "threads": threads
+                "bounds": f"{bbox}",
+                "filename": ept,
+                "type": "readers.ept",
+                "tag": "readdata",
+                "spatialreference": srs,
+                "threads": threads,
             },
-            {
-            "type":"filters.outlier",
-            "method":"radius",
-            "radius":1.0,
-            "min_k":4
-            },
-            {
-            "type":"filters.range",
-            "limits":"returnnumber[1:1]"
-            },
-            {
-            "type": "writers.las",
-            "filename": out_path,
-            "a_srs": srs
-            }
-        ]}
-    return(pipe) 
-    
+            {"type": "filters.outlier", "method": "radius", "radius": 1.0, "min_k": 4},
+            {"type": "filters.range", "limits": "returnnumber[1:1]"},
+            {"type": "writers.las", "filename": out_path, "a_srs": srs},
+        ]
+    }
+    return pipe
 
-    #pipeline = pdal.Pipeline(pipe)
-    #if pipeline.validate():
-    #return(pipeline)
-    #else:
+    # pipeline = pdal.Pipeline(pipe)
+    # if pipeline.validate():
+    # return(pipeline)
+    # else:
     #    raise Exception('Bad pipeline (sorry to be so ambigous)!')
 
 
-
-
-if __name__ == '__main__':
-    '''Returns subsets of supplied files clipped to bbox supplied in command or multiple bboxs specified in file using --bbxf '''
+if __name__ == "__main__":
+    """Returns subsets of supplied files clipped to bbox supplied in command or multiple bboxs specified in file using --bbxf"""
 
     # parse args
     parser = argparse.ArgumentParser()
-    parser.add_argument('--bbox', type=str, required=False,
-    help='''The extents of the resource to select in 2 dimensions, expressed as a string,
-    in the format: '([xmin, xmax], [ymin, ymax])' ''')
-    parser.add_argument('--bbxf', type=str, required=False, help='''path to file with a bbox on each line''') 
-    parser.add_argument('--ept', type=str, required=True, help='path to ept')
-    parser.add_argument('--srs', type=str, required=True, help='EPSG code of srs of files, all files must be in the same coordinate system')
-    parser.add_argument('--out', type=str, required=True, help='path to output directory')
-    args = parser.parse_args() 
+    parser.add_argument(
+        "--bbox",
+        type=str,
+        required=False,
+        help="""The extents of the resource to select in 2 dimensions, expressed as a string,
+    in the format: '([xmin, xmax], [ymin, ymax])' """,
+    )
+    parser.add_argument(
+        "--bbxf",
+        type=str,
+        required=False,
+        help="""path to file with a bbox on each line""",
+    )
+    parser.add_argument("--ept", type=str, required=True, help="path to ept")
+    parser.add_argument(
+        "--srs",
+        type=str,
+        required=True,
+        help="EPSG code of srs of files, all files must be in the same coordinate system",
+    )
+    parser.add_argument(
+        "--out", type=str, required=True, help="path to output directory"
+    )
+    args = parser.parse_args()
 
     # make list off bboxes
     if args.bbox:
         bboxes = [args.bbox]
     elif args.bbxf:
         bboxes = []
-        #try:
+        # try:
         with open(args.bbxf) as bxs:
             for line in bxs:
                 bboxes.append(line)
     else:
-        print('One must either specify --bbox or --bbxf')
+        print("One must either specify --bbox or --bbxf")
 
     for bbox in bboxes:
         # unpack the bbox string
-        xmin, xmax, ymin, ymax = bbox.strip('()').replace('[', '').replace(']','').split(',')
+        xmin, xmax, ymin, ymax = (
+            bbox.strip("()").replace("[", "").replace("]", "").split(",")
+        )
         xmin = float(xmin)
         xmax = float(xmax)
         ymin = float(ymin)
         ymax = float(ymax)
 
-        #make a laz for the window from ept.
+        # make a laz for the window from ept.
         ept_window_query(xmin, xmax, ymin, ymax, args.ept, args.srs, args.out, tag=None)
--- start_ept.sh
+++ start_ept.sh
@@ -16,6 +16,6 @@ EPT=$2
 BBOX=$3
 SRS=$4
 
-docker run --rm -it -v $PWD:/work  -v $OUTPATH:/out -v -u $(id -u):$(id -g) -e HOME=/work -w /work quay.io/kulpojke/bbox_query:ept-sha-45426f6 $BBOX $SRS $EPT
+docker run --rm -it -v $PWD:/work  -v $OUTPATH:/out -u $(id -u):$(id -g) -e HOME=/work -w /work quay.io/kulpojke/bbox_query:ept-sha-8167227 $BBOX $SRS $EPT
 
-#./start.sh ~/Downloads /media/data/AOP data/chm.vrt data/dtm.vrt data/dsm.vrt entwine/ '([319864,319925],[4096389,4096442])' EPSG:26911
+#./start.sh ~/Downloads https://storage.googleapis.com/monument_bucket/carr/entwine/ept.json 
