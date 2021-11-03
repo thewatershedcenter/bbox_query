cf92b348923214003307f9d25964b2e651b7a379
michael <michaelhuggins@protonmail.com>
2021-11-03 09:42:31 -0700

    added code_test.py

---
+++ code_tests.py
@@ -0,0 +1,64 @@
+#%%
+from importlib import reload
+import app_ept
+from app_ept import *
+reload(app_ept)
+from app_ept import *
+
+vector='/media/data/WRTC/bbox_query/test/test_buff.shp'
+
+def xy_test(to_srs, vector=vector, layer=None):
+    '''
+    Tests to see if points are within bounds of
+    the tile which containe the test polygon.
+    '''
+
+    transform = get_transform(vector, to_srs)
+    minx, maxx, miny, maxy = bbox_from_vector(vector, layer=layer)
+
+    minx, miny, minz = transform.TransformPoint(miny, minx)
+    maxx, maxy, maxz = transform.TransformPoint(maxy, maxx)
+
+    ref = {
+        "maxx": 489749.99,
+        "maxy": 4514999.99,
+        "maxz": 954.45,
+        "minx": 489000,
+        "miny": 4514250,
+        "minz": 245.61
+    }
+
+    test = {
+        "maxx": maxx < ref['maxx'],
+        "maxy": maxy < ref['maxy'],
+        "minx": minx > ref['minx'],
+        "miny": miny > ref['miny']
+    }
+
+    return test
+
+
+xy_test(6339)
+
+
+# %%
+import pandas as pd
+import matplotlib.pyplot as plt
+
+usgs = pd.DataFrame({'x': [489000, 489000, 489749.99, 489749.99],
+                  'y': [4514250, 4514999.99, 4514999.99, 4514250]})
+
+geometry = gpd.points_from_xy(usgs['x'], usgs['y'])
+
+usgs = gpd.GeoDataFrame(usgs, geometry=geometry, crs=6339)
+
+s = transform_vector(vector, srs)
+
+# create the plot
+fig, ax = plt.subplots(figsize=(12, 8))
+usgs.plot(ax=ax)
+s.plot(ax=ax)
+
+# %%
+
+# %%
