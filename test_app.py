d7b4d6e07c2f846c18f9d7b3b1e1f1854b7f6131
michael <michaelhuggins@protonmail.com>
2021-11-04 14:12:48 -0700

    added new test to test_app.py with condition meant to fail, as test of test

--- test_app.py
+++ test_app.py
@@ -1,5 +1,7 @@
 from app_ept import *
 import pandas as pd
+import geopandas as gpd
+from shapely import geometry as gm
 
 vector = '/test/test_buff.shp'
 
@@ -13,3 +15,20 @@ usgs = pd.DataFrame({'x': [489000, 489000, 489749.99, 489749.99],
 def test_get_ept_srs():
     ept = 'https://storage.googleapis.com/monument_bucket/CarrHirzDelta_1/entwine/ept.json'
     assert get_ept_srs(ept) == 'EPSG:6339'
+
+
+def test_transform_vector():
+    # bounds of test tile the shape falls in 
+    usgs = pd.DataFrame({'x': [489000, 489000, 489749.99, 489749.99],
+                     'y': [4514250, 4514999.99, 4514999.99, 4514250]})
+
+    # make points 
+    points = gpd.points_from_xy(usgs['x'], usgs['y'])
+
+    # make polygon
+    poly = gm.Polygon([[p.x, p.y] for p in usgs.geometry])
+
+    # get transformed polygon of interest
+    s = transform_vector(vector, srs)
+
+    assert poly.contains(s.geometry.values[0]) == 'pig'
