824e176cfe9fde6de94e99b6a985e36ceb97266e
michael <michaelhuggins@protonmail.com>
2021-11-10 10:50:09 -0800

    testing

--- app_ept2.py
+++ app_ept2.py
@@ -24,6 +24,7 @@ def get_ept_srs(ept):
 def read_and_transform_vector(vector, srs):
     # read the vector file
     s = gpd.read_file(vector)
+
     # get the integer from the srs
     srs_number = int(srs.split(':')[-1])
 
@@ -36,16 +37,151 @@ def read_and_transform_vector(vector, srs):
     return(s)
 
 
-if __name__ == '__main__':
-    print(print(pdal.__version__))
-
-    '''
-    s = read_and_transform_vector(vector, srs)
-
+def make_bbox(geodf):
+    '''returns bbox of a geodf in pdal format'''
     # get the bbox from the vector
-    x, y = s.geometry.envelope.exterior.values[0].coords.xy
+    x, y = geodf.geometry.envelope.exterior.values[0].coords.xy
     minx, maxx, miny, maxy = min(x), max(x), min(y), max(y)
 
     # pack up the bbox
     box = ([minx, maxx], [miny, maxy])
-    '''
+
+    return(box)
+
+
+def optimal_box_division_size():
+    '''TODO: firgure out a way to optimize subbox size for
+    any pointcloud, using dryruns?'''
+
+
+def divide_bbox(box, size):
+    '''divides box into subboxes of roughly size x size (m)'''
+
+    # unpack the box
+    ([minx, maxx], [miny, maxy]) = box
+
+    # calculate x edges of tiles
+    x = minx
+    xs = []
+    while x < maxx:
+        xs.append([x, x + size])
+        x = x + size
+    if x < maxx:
+        xs.append([x, maxx])
+
+    # calculate y edges of tiles
+    y = miny
+    ys = []
+    while y < maxy:
+        ys.append([y, y + size])
+        y = y + size
+    if y < maxy:
+        ys.append([y, maxy])
+
+    # now use x and y edges to define tile corners
+    bxs = []
+    for x in xs:
+        for y in ys:
+            sub_box = (x, y)
+            bxs.append(sub_box)
+
+    return(bxs)
+
+def make_pipe(ept, bbox, srs):
+    '''Creates, validates and then returns the pdal pipeline'''
+
+    # pipe as dict
+    pipe = {
+        'pipeline': [
+            {
+                'bounds': f'{bbox}',
+                'filename': ept,
+                'type': 'readers.ept',
+                'spatialreference': srs
+            },
+            {
+                'type': 'filters.elm'
+            },
+            {
+                'type': 'filters.assign',
+                'assignment': 'Classification[:]=0',
+                'where': 'Classification > 20'
+            },
+            {
+                'type': 'filters.outlier',
+                'method': 'radius',
+                'radius': 1.0,
+                'min_k': 6
+            },
+            {
+                'type': 'filters.hag_nn',
+                'count': 2
+            },
+            {
+                'type': 'filters.range',
+                'limits': 'HeightAboveGround[0:88]'
+            }
+        ]
+    }
+
+    # make pipe into pdal pipe thing
+    pipeline = pdal.Pipeline(json.dumps(pipe))
+
+    # vlidate and return or raise complaint
+    if pipeline.validate():
+        return(pipeline)
+    else:
+        raise Exception('Bad pipeline (sorry to be so ambigous)!')
+
+
+def parse_arguments():
+    '''parses the arguments, returns args'''
+
+    # init parser
+    parser = argparse.ArgumentParser()
+
+    # add args
+    parser.add_argument(
+        '--vector',
+        type=str,
+        required=False,
+        help='Path to vector file for which points will be returned.',
+    )
+
+    parser.add_argument('--ept', type=str, required=True, help='path to ept')
+
+    parser.add_argument(
+        '--out', type=str, required=True, help='path to output directory'
+    )
+
+    # parse the args
+    args = parser.parse_args()
+
+    return(args)
+
+
+if __name__ == '__main__':
+    print(print(pdal.__version__))
+
+    # parse the args
+    args = parse_arguments()
+
+    # get srs
+    srs = get_ept_srs(args.ept)
+
+    # read vector to geodf
+    s = read_and_transform_vector(args.vector, srs)
+
+    # find bbox of s
+    box = make_bbox(s)
+
+    # define sub box size, TODO: getthis from a function
+    size = 3_500
+
+    # make list of sub-boxes
+    divide_bbox(box, size)
+
+
+
+
+
--- test_app.py
+++ test_app.py
@@ -9,7 +9,7 @@ import json
 import os
 
 
-vector1 = 'buffers/test_buff.shp'
+vector1 = 'test/test_buff.shp'
 
 vector2 = 'test/'
 
@@ -22,11 +22,12 @@ os.makedirs(out, exist_ok=True)
 global args
 args = Namespace(vector=vector1, ept=ept, out=out)
 
-#  %%
 
 def test_get_ept_srs():
     '''tests that srs can be retrieved'''
     srs = get_ept_srs(args.ept)
+    print(srs)
+
     assert srs == 'EPSG:6339'
 
 
@@ -35,8 +36,10 @@ args.hesher = abs(hash(args.ept)) % (10 ** 8)
 args.srs = get_ept_srs(args.ept)
 
 
-def test_transform_vector():
+def test_read_and_transform_vector():
     '''tests that vector file can be read and transformed to srs'''
+
+    srs = get_ept_srs(args.ept)
 
     # bounds of test tile the shape falls in 
     usgs = pd.DataFrame({'x': [489000, 489000, 489749.99, 489749.99],
@@ -49,6 +52,49 @@ def test_transform_vector():
     poly = gm.Polygon([[p.x, p.y] for p in points])
 
     # get transformed polygon of interest
-    s = read_and_transform_vector(args.vector, args.srs, 'arbitrary_string')
+    s = read_and_transform_vector(args.vector, srs)
+
+    print(poly.contains(s.geometry.values[0]))
 
     assert poly.contains(s.geometry.values[0])
+
+
+def test_make_box_ALSO_divide_bbox_ALSO_make_pipe():
+    '''tests both make_box and divide_bbox'''
+    # get srs
+    srs = get_ept_srs(args.ept)
+
+    # get transformed polygon of interest
+    s = read_and_transform_vector(args.vector, srs)
+
+    # find bbox of s
+    box = make_bbox(s)
+
+    assert box == ([489013.26329637936, 489116.2366349314],
+                   [4514616.760521351, 4514838.612628906])
+    print('Succesfully tested make_bbox!')
+
+    size = 10
+
+    bxs = divide_bbox(box, size)
+
+    print(len(bxs))
+    assert len(bxs) == 253
+    print('Succesfully tested divide_bbox!')
+
+    pipe = make_pipe(args.ept, bxs[2], srs)
+    print(type(pipe))
+
+    count = pipe.execute()
+    print(count)
+
+
+# %%
+test_get_ept_srs()
+
+# %%
+test_read_and_transform_vector()
+
+# %%
+test_make_box_ALSO_divide_bbox_ALSO_make_pipe()
+# %%
