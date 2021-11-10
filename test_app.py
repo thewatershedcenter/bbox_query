1c70ce45f839d18f944ea1550fc9a0af58d055a3
michael <michaelhuggins@protonmail.com>
2021-11-10 08:11:00 -0800

    fixed matrix in build yaml? changed tags, changed test_app to test app_ept2

--- test_app.py
+++ test_app.py
@@ -9,7 +9,7 @@ import json
 import os
 
 
-vector1 = 'buffers/fid_17.gpkg'
+vector1 = 'buffers/test_buff.shp'
 
 vector2 = 'test/'
 
@@ -22,13 +22,7 @@ os.makedirs(out, exist_ok=True)
 global args
 args = Namespace(vector=vector1, ept=ept, out=out)
 
-# add additional args
-args.hesher = abs(hash(args.ept)) % (10 ** 8)
-args.srs = get_ept_srs(args.ept)
-
-
 #  %%
-
 
 def test_get_ept_srs():
     '''tests that srs can be retrieved'''
@@ -36,13 +30,13 @@ def test_get_ept_srs():
     assert srs == 'EPSG:6339'
 
 
-# %%
+# add additional args
+args.hesher = abs(hash(args.ept)) % (10 ** 8)
+args.srs = get_ept_srs(args.ept)
 
 
 def test_transform_vector():
     '''tests that vector file can be read and transformed to srs'''
-    # get srs
-    srs = get_ept_srs(args.ept)
 
     # bounds of test tile the shape falls in 
     usgs = pd.DataFrame({'x': [489000, 489000, 489749.99, 489749.99],
@@ -58,69 +52,3 @@ def test_transform_vector():
     s = read_and_transform_vector(args.vector, args.srs, 'arbitrary_string')
 
     assert poly.contains(s.geometry.values[0])
-
-
-# %%
-
-
-def test_fetch_points_file():
-    '''tests when vector points to single file'''
-
-    # get bbox, fname
-    bbox, fname = bbox_from_vector(args.vector, args.srs, args.hesher)
-    print(args.vector, fname)
-    # put into the bboxes list
-    bboxes = [bbox]
-    fnames = [fname]
-
-    # download the pointcloud
-    query_from_list(bboxes, args.srs, args.out, args.vpath, fnames, args.ept)
-
-    # make sure there are points
-    cmd = f'pdal info {args.out}/{fname}.las'
-    result = subprocess.run(cmd, shell=True, capture_output=True)
-    count = json.loads(result.stdout.decode("utf-8"))['stats']['statistic'][0]['count']
-    print(count)
-    assert count > 100
-
-
-# %%
-
-
-# change the vector to just dir
-args.vector = vector2
-
-
-def test_fetch_points_dir():
-    '''tests when vector points to dir'''
-
-    # empty list for boxes
-    bboxes = []
-    fnames = []
-
-    # ls the dector_dir
-    vectors = [os.path.join(args.vector, f)
-                   for f in os.listdir(vector2)
-                   if f.endswith('.gpkg')  
-                   or f.endswith('.shp') 
-                   or f.endswith('.geojson')]
-
-    for vector in vectors:
-        bbox, fname = bbox_from_vector(vector, args.srs, args.hesher)
-        bboxes.append(bbox)
-        fnames.append(fname)
-
-    # download the pointcloud
-    query_from_list(bboxes, args.srs, args.out, args.vpath, fnames, args.ept)
-
-    # make sure there are points
-    for fname in fnames:
-        cmd = f'pdal info {args.out}/{fname}.las'
-        result = subprocess.run(cmd, shell=True, capture_output=True)
-        count = json.loads(result.stdout.decode("utf-8"))['stats']['statistic'][0]['count']
-        print(count)
-        assert count > 100
-    
-# %%
-
-# %%
